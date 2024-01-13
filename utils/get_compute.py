154452ed03f779a0dd3c7cc41f91cda0e221fc5d
Samuel Simko <samuel.simko@cern.ch>
2024-01-13 11:16:57 +0100

    scaling_mlps: remove unused files

--- utils/get_compute.py
+++
@@ -1,9 +0,0 @@
-import torch
-from fvcore.nn import FlopCountAnalysis
-
-
-def get_compute(model, dataset_size, res):
-    input = torch.randn(1, 3 * res * res).cuda()
-    flops = FlopCountAnalysis(model, input)
-
-    return flops.total() * 3 * dataset_size
